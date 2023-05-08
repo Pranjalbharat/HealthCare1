aa084cef5e3c80d491b6cdab152198b12a2f2b81
Shubham Puhal <shubhampuhal01@gmail.com>
2023-05-08 22:42:40 +0530

    HomePage fixed, Test Button activated, Default page changed to login.

--- app/src/main/java/com/example/healthcare/HomePage.java
+++ app/src/main/java/com/example/healthcare/HomePage.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomePage extends AppCompatActivity {
     private ImageButton Doctor_profile;
     private ImageButton Chat_message;
+    private Button Test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,13 @@ public class HomePage extends AppCompatActivity {
 
         Doctor_profile=findViewById(R.id.imageview);
         Chat_message=findViewById(R.id.chat_button);
+        Test = findViewById(R.id.center_button);
 
 
         Doctor_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(HomePage.this, Doctor_profile.class));
-                finish();
             }
         });
 
@@ -33,9 +35,14 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(HomePage.this, Chat_page.class));
-                finish();
             }
         });
 
+        Test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this,Testpage.class));
+            }
+        });
     }
 }
